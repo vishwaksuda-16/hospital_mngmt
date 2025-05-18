6edf788c54046de6d3833f9aeccf8823055e8644
230701385 <230701385@rajalakshmi.edu.in>
2025-05-18 18:21:25 +0530

    Added Calendar(not functional)

--- twilioUtils.js
+++ twilioUtils.js
@@ -1,10 +1,5 @@
 require('dotenv').config();
 
-console.log("DEBUG ENV:");
-console.log("TWILIO_ACCOUNT_SID:", process.env.TWILIO_ACCOUNT_SID);
-console.log("TWILIO_AUTH_TOKEN:", process.env.TWILIO_AUTH_TOKEN);
-console.log("TWILIO_PHONE_NUMBER:", process.env.TWILIO_PHONE_NUMBER);
-
 const twilio = require('twilio');
 const schedule = require('node-schedule');
 
